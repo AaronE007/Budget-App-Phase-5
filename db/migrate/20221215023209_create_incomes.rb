a7ce9c5473816b9b3f1d95af4d3ea0c19908e90c
Aaron Elias <aaronelias007@gmail.com>
2022-12-18 20:17:41 -0500

    Add assocations to the db model

--- db/migrate/20221215023209_create_incomes.rb
+++ db/migrate/20221215023209_create_incomes.rb
@@ -4,6 +4,9 @@ class CreateIncomes < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :type
       t.integer :amount
+      t.string :time_period
+      t.references :user, foreign_key: true, null: false
+      t.references :income_category, foreign_key: true, null: false
 
       t.timestamps
     end
--- db/migrate/20221215023542_create_user_bills.rb
+++ db/migrate/20221215023542_create_user_bills.rb
@@ -4,6 +4,9 @@ class CreateUserBills < ActiveRecord::Migration[6.1]
       t.string :name
       t.string :description
       t.integer :amount
+      t.references :user, foreign_key: true, null: false
+      t.references :expense_category, foreign_key: true, null: false
+
 
       t.timestamps
     end
