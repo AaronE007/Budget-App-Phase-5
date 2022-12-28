15b59913c583c7649b43dc154af5c17be5c7627b
Aaron Elias <aaronelias007@gmail.com>
2022-12-27 20:22:20 -0500

    Finish user migration

--- db/migrate/20221227234133_create_users.rb
+++ db/migrate/20221227234133_create_users.rb
@@ -3,6 +3,9 @@ def change
   create_table :users do |t|
     t.string :username
     t.string :email
+    t.string :password_digest
+    t.string :bio
+
 
     t.timestamps
   end
---
+++ db/migrate/20221227235655_create_user_bills.rb
@@ -0,0 +1,15 @@
+class CreateUserBills < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_bills do |t|
+      t.string :name
+      t.string :description
+      t.integer :amount
+      t.references :user, foreign_key: true, null: false
+      t.references :expense_category, foreign_key: true, null: false
+  
+  
+      t.timestamps
+    end
+  end
+ end
+ 
