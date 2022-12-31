5ec82bceda06488cabb09016890bf80012dbe59c
Aaron Elias <aaronelias007@gmail.com>
2022-12-30 21:44:19 -0500

    Add home page to front end

---
+++ client/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import { UserContext } from '../context/user'
+
+export default function Home() {
+   const { user } = useContext(UserContext)
+
+  if  (!user || user.error || !user.username) {
+    return(<h1> Please Navigate to Login or Sign Up Form</h1>)
+  } else{
+      return (
+        <div>
+          <h2>Home Page for {user.username}</h2>
+          <h4>{user.bio}</h4>
+        </div>
+      )
+  }
+}
