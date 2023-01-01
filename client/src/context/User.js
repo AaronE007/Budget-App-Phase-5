ccf58a44f4fe6ee3f2ffa6f15b429450b0f101e4
Aaron Elias <aaronelias007@gmail.com>
2023-01-01 18:57:12 -0500

    Add useEffect for /me to useEffect

--- client/src/context/User.js
+++ client/src/context/User.js
@@ -6,14 +6,37 @@ const UserContext = React.createContext();
 const UserProvider = ({children}) => {
 
   const[user, setUser ] = useState({});
-  const[loggeddIn, setLoggedIN] = useState([])
+  const[loggeddIn, setLoggedIn] = useState([])
 
+  useEffect(() => {
+    fetch('/me')
+    .then(res => res.json())
+    .then(data => {
+      setUser(data) 
+        if (data.error) {
+        setLoggedIn(false)
+      }else {
+        setLoggedIn(true)
+      }
+    })
+  }, [])
+
+  const login = (user) => {
+    setUser(user)
+    setLoggedIn(true)
+   
+  }
+
+  const logout = () => {
+    setUser({})
+    setLoggedIn(false)
+  }
  
 
   return (
-    <div>
-      
-    </div>
+    <UserContext.Provider value={{user, loggeddIn, login, logout, signup}}>
+      {children}
+    </UserContext.Provider>
   )
 }
 
