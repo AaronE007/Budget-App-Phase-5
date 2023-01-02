7bc85d6d0bbcffe6f252d00c2ff63cc42e4d63db
Aaron Elias <aaronelias007@gmail.com>
2023-01-01 19:17:21 -0500

    Add routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
+import React from "react";
+import {Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./components/Footer";
+import SignUpForm from "./components/SignUpForm";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<LoginForm/>} />
+        <Route path="/signup" element={<SignUpForm/>} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
