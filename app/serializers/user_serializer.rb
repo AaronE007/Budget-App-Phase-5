98f742e89894432400da0310bfddc27731e072b5
Aaron Elias <aaronelias007@gmail.com>
2023-01-02 18:37:42 -0500

    Add user serializer and remove resource for user routes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email
+  attributes :id, :username, :email, :bio
+
+  has_many :incomes
+  has_many :income_categories, through: :incomes
+  
+  has_many :user_bills
+  has_many :expense_categories, through: :user_bills
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  
   resources :expense_categories
   resources :user_bills
   resources :income_categories
   resources :incomes
-  resources :users
-
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  
 end
+
