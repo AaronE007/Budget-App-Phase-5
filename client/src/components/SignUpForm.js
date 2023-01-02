c79e0f4aa812933ac67286d4f8d9e7dd6c6e90a1
Aaron Elias <aaronelias007@gmail.com>
2023-01-01 19:22:00 -0500

    Add state to SignUpForm

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -2,3 +2,12 @@ import React,{ useState, useContext } from "react";
 import {UserContext} from "../context/user";
 import {useNavigate} from "react-router-dom"
 
+const SignUpForm = () => {
+  const {signup} = useContext(UserContext)
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [bio, setBio] = useState("");
+  const [errors, setErrors] = useState([]);
+  const navigate = useNavigate()
