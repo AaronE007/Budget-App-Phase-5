f8f06c2f2cb34243c39a84e3a545fd1ec5934c23
Aaron Elias <aaronelias007@gmail.com>
2023-01-01 19:23:33 -0500

    Add handle submit to signup form

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -11,3 +11,31 @@ const SignUpForm = () => {
   const [bio, setBio] = useState("");
   const [errors, setErrors] = useState([]);
   const navigate = useNavigate()
+
+function handleSubmit(e) {
+  e.preventDefault();
+  fetch("/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"},
+      body: JSON.stringify({
+        user: {
+          username: username,
+          email: email,
+          password: password,
+          password_confirmation:  passwordConfirmation,
+          bio: bio
+        }
+      })
+    })
+    .then(res => res.json())
+    .then(user => {
+      if (!user.errors) {
+        signup(user)
+        navigate('/')
+      } else {
+        const errorsList = user.errors.map(e => <li>{e}</li>)
+        setErrors(errorsList)
+      }
+    } )
+  }
