449c9f7359d414152321cf6c2e3fc3fc8b0e6d33
Aaron Elias <aaronelias007@gmail.com>
2023-01-01 19:32:50 -0500

    Add User lout fetch

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,3 +2,29 @@ import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../context/user'
 import {useNavigate} from "react-router-dom"
+
+const style = {
+  width: "100%",
+  magrin:"auto",
+  padding: "1em",
+  color: "black",
+  backgroundColor: "#8ec3eb",
+  fontWeight: "600",
+  verticalAlign: "center",
+}
+
+const Navbar = () => {
+
+  const {user, logout, loggedIn} = useContext(UserContext)
+  const navigate = useNavigate()
+
+  const userLogout = () => {
+    fetch('/logout', {
+      method: 'DELETE',
+      headers: {"Content-Type": 'application/json'}
+    })
+    .then(() => {
+      logout()
+      navigate('/')
+    })
+  }
