772e44e0e4471b3360978f0f96da78fe85374a55
Aaron Elias <aaronelias007@gmail.com>
2023-01-01 19:25:32 -0500

    Add signup form

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -39,3 +39,38 @@ function handleSubmit(e) {
       }
     } )
   }
+
+  return (
+    <div>
+    <h1>Sign Up</h1>
+    <form onSubmit={handleSubmit} style={{margin: "auto", border: "solid", backgroundColor: "#8ec3eb" , height: 300, width: 400, color: "#2a6592"}}>
+      <label>Username: </label>
+      <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <br/>
+        <br/>
+      <label>Email: </label>
+      <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+        <br/>
+        <br/>
+      <label>Password: </label>
+      <input type="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        <br/>
+        <br/>
+        <label>Password Confirm: </label>
+        <input onChange={(e) => setPasswordConfirmation(e.target.value)} type="password" id="password_confirmation" value={passwordConfirmation}  required/>
+        <br/>
+        <br/>
+      <label>Bio: </label>
+      <input type="text" id="bio" value={bio} onChange={(e) => setBio(e.target.value)} required/>
+        <br/>
+        <br/>
+      <input type="submit" value="Add User" />
+    </form>
+      <ul>
+    {errors}
+    </ul>
+  </div>
+  )
+}
+
+export default SignUpForm
