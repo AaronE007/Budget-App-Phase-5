5b5fc27ad9be1e5184082437bda3f94f2fc641e1
Aaron Elias <aaronelias007@gmail.com>
2023-01-07 21:56:19 -0500

    delete namespace from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
   resources :expense_categories
   resources :user_bills
   resources :income_categories
@@ -8,8 +7,7 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  end 
-  
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
 
