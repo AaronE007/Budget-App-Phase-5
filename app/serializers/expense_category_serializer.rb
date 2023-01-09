a1a09d50449d68158dcf116379bc97820dc08904
Aaron Elias <aaronelias007@gmail.com>
2023-01-09 17:52:20 -0500

    Add custom rendering route

--- app/serializers/expense_category_serializer.rb
+++ app/serializers/expense_category_serializer.rb
@@ -4,3 +4,34 @@ class ExpenseCategorySerializer < ActiveModel::Serializer
   has_many :user_bills
   has_many :users, through: :user_bills
 end
+
+
+# {
+# "id": 1,
+# "username": "Aaron",
+# "email": "aaron@aaronmail.com",
+# "bio": "Stock guy",
+#   "incomes": [
+#     {
+#     "id": 1,
+#     "name": "Teacher Pay Check",
+#     "amount": "3.5",
+#     "time_period": "Bi-Weekly"
+#     }
+#   ],
+  
+#   ],
+#   "expense_categories": [
+#     {
+#     "id": 1,
+#     "name": "House Hold Bill"
+#     }
+#     "user_bills": [
+#     {
+#     "id": 1,
+#     "name": "Internet Bill",
+#     "description": "Got dang interwebs money.",
+#     "amount": "500.4"
+#     }
+#     ]
+#   }
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,30 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email, :bio
+  attributes :id, :username, :email, :bio, :expense_categories
 
   has_many :incomes
-  
-  has_many :user_bills
-  has_many :expense_categories, through: :user_bills
 
+  def expense_categories
+    object.expense_categories.includes(:user_bills).map do |ec|
+    {
+      "id": ec.id,
+      "name": ec.name,
+      "user_bills": ec.user_bills
+    } 
+    end 
+  end 
 end
+
+
+
+# "expense_categories": [
+# {
+#   "id": 1,
+#   "name": "House Hold Bill",
+#   "user_bills": [
+#   {
+#   "id": 1,
+#   "name": "Internet Bill",
+#   "description": "Got dang interwebs money.",
+#   "amount": "500.4"
+#   }
+#   ]
