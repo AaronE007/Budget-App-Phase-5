e15cce989e5f568ca9fe528cbe2a62bc5597efe7
Aaron Elias <aaronelias007@gmail.com>
2023-01-09 18:20:08 -0500

    Add income card design

--- client/src/components/ExpenseCatCard.js
+++ client/src/components/ExpenseCatCard.js
@@ -3,8 +3,8 @@ import React from 'react'
 const ExpenseCatCard = ({expenseCategory}) => {
   return (
     <div style={{margin: "auto", border: "solid", backgroundColor: "#2a6592", height: 200, width:400, color: "#db9833"}}>
-    <h3>Name: {expenseCategory.name}</h3>
-</div>
+      <h3>Name: {expenseCategory.name}</h3>
+    </div>
   )
 }
 
--- client/src/components/IncomeCard.js
+++ client/src/components/IncomeCard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const IncomeCard = () => {
+const IncomeCard = ({income}) => {
+  
   return (
-    <div>
-      
-    </div>
+  <div style={{margin: "auto", border: "solid", backgroundColor: "#2a6592", height: 200, width:400, color: "#db9833"}}>
+    <h3>Name: {income.name}</h3>
+    <h4>Amount: {income.amount}</h4>
+    <h4>Timer Period: {income.time_period}</h4>
+  </div>
   )
 }
 
 export default IncomeCard
+
+
