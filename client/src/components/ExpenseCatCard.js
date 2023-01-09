edafb5f777055632344770f692c982e9b0cd2aac
Aaron Elias <aaronelias007@gmail.com>
2023-01-09 18:39:48 -0500

    Add react card state

--- client/src/components/ExpenseCatCard.js
+++ client/src/components/ExpenseCatCard.js
@@ -3,6 +3,10 @@ import ReactCardFlip from 'react-card-flip';
 
 
 const ExpenseCatCard = ({expenseCategory}) => {
+
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  
   return (
     <div style={{margin: "auto", border: "solid", backgroundColor: "#2a6592", height: 200, width:400, color: "#db9833"}}>
       <h3>Name: {expenseCategory.name}</h3>
