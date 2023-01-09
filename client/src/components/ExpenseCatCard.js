e149fa9f252990e4f5937c8362e57ad1c61449c9
Aaron Elias <aaronelias007@gmail.com>
2023-01-09 18:47:33 -0500

    Add card flip logic to expense cat card

--- client/src/components/ExpenseCatCard.js
+++ client/src/components/ExpenseCatCard.js
@@ -6,12 +6,30 @@ const ExpenseCatCard = ({expenseCategory}) => {
 
   const [isFlipped, setIsFlipped] = useState(false);
 
-  
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  }
+
   return (
-    <div style={{margin: "auto", border: "solid", backgroundColor: "#2a6592", height: 200, width:400, color: "#db9833"}}>
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+    <div style={{margin: "auto", border: "solid", backgroundColor: "#2a6592", height: 200, width:300, color: "peachpuff"}}>
+    <button onClick={handleDeleteClick}>Delete</button>
       <h3>Name: {expenseCategory.name}</h3>
+      <br/>
+      <button onClick={handleClick}>Click to see User Bills</button>
     </div>
+    <div style={{margin: "auto", border: "solid", backgroundColor: "peachpuff", height: 600, width: 300, color: "#2a6592"}}>
+      <div>
+        <BillList /> 
+      </div>
+      <br/>
+        <button onClick={handleClick}>Click to Return to Category</button>
+    </div>
+  </ReactCardFlip>
+    
   )
 }
 
 export default ExpenseCatCard
+
+
