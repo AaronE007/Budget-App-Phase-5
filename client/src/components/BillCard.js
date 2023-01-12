26e9afea539d4b36f11c6e19e0f535531f0eb43f
Aaron Elias <aaronelias007@gmail.com>
2023-01-11 23:14:49 -0500

    Add props to bill card

--- client/src/components/BillCard.js
+++ client/src/components/BillCard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const BillCard = () => {
+const BillCard = ({bill}) => {
   return (
-    <div>
-      
-    </div>
+    <div style={{margin: "auto", border: "solid", backgroundColor: "#2a6592", height: 200, width:200, color: "#db9833"}}>
+      <h3>Name: {bill.name}</h3>
+      <h4>Amount: {bill.amount}</h4>
+      <h4>Description: {bill.description}</h4>
+  </div>
   )
 }
 
--- client/src/components/BillList.js
+++ client/src/components/BillList.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const BillList = ({bills}) => {
 
-  const renderBills = bills.map(bill => <BillCard key={bill.id} expenseCategory={expenseCategory}/>)
+  const renderBills = bills.map(bill => <BillCard key={bill.id} bill={bill}/>)
   
   return (
     <div>
