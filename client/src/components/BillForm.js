fa5912d8250dd1fb7839300e9abf57a1c9464b82
Aaron Elias <aaronelias007@gmail.com>
2023-01-14 21:05:45 -0500

    Add bill from based off of template

---
+++ client/src/components/BillForm.js
@@ -0,0 +1,62 @@
+import { useState, useContext } from "react"
+import React  from 'react'
+import { UserContext } from "../context/user"
+
+const StockForm = () => {
+  const {addBill} = useContext(UserContext)
+  const [userbill, setBill] = useState({
+    name: "", 
+    description: "",
+    amount: ""
+  })
+
+  const handleChange = (e) => {
+    setBill({
+      ...userbill,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const newBill = {
+    name: userbill.name,
+    description: userbill.description,
+    amount: userbill.amount,
+  }
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newBill),
+   })
+    .then(res => res.json())
+    .then(userbill => {
+          addBill(userbill)
+     })
+  } 
+
+  return (
+  <div>
+    <form onSubmit={handleSubmit} style={{margin: "auto", border: "solid", backgroundColor: "#8ec3eb" , height: 600, width: 400, color: "#2a6592"}}>
+      <div>
+        <h5>Add a New Bill Here.</h5>
+      </div>
+      <label>Name: </label>
+      <input onChange={handleChange} type="text" name="name" value={userbill.name} required/>
+        <br/>
+      <label>Descriptiont: </label>
+      <input onChange={handleChange} type="text" name="desciption" value={userbill.description} required/>
+        <br/>
+      <label>Amount: </label>
+      <input onChange={handleChange} type="number" name="Amount" value={userbill.amount} required/>
+        <br/>
+      <input type="submit" value="Add Bill" />
+    </form>
+  </div>
+  )
+}
+
+export default StockForm
