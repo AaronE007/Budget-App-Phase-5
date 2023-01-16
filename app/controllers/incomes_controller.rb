57c48cf3cde4c87fabac92a4ef9ec76a8349b3e1
Aaron Elias <aaronelias007@gmail.com>
2023-01-15 19:25:17 -0500

    Fix error in create for income variable

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -8,13 +8,13 @@ class IncomesController < ApplicationController
 
   # POST /incomes
   def create
-    income = current_user.create!(income_params)
+    income = current_user.incomes.create!(income_params)
     render json: income, status: :created
   end
 
   # PATCH/PUT /incomes/1
   def update
-   if income = current.user.incomes.find(params[:id])
+   if income = current_user.incomes.find(params[:id])
     income.update!(income_params)
     income.reload
     render json: income
